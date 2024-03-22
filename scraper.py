d9c018a363bfb4ad729ee0d270dc77da9e3ea16c
Rafael Veiga <114686425+Rveiga84@users.noreply.github.com>
2024-03-22 15:45:47 -0400

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_stories_by_votes(hnlist):
+  return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+#this is a common pattern to use when you want to sort a list of dictionaries by a key in each dictionary. Reverse is a boolean that tells the sort function to sort in reverse order.
+
+def create_custom_hn(links, subtext):
+  hn = []
+  for idx, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[idx].select('.score')
+    if len(vote):
+      points = int(vote[0].getText().replace(' points', ''))
+      if points > 99:
+        hn.append({'title': title, 'link': href, 'votes': points})
+  return sort_stories_by_votes(hn)
+
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
